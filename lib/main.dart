cdfef65a8218af0b9ebfb840336d5c2791520f4f
MarcMalha <marc.malha@beanstock.com>
2025-02-01 19:09:31 +0100

    Chore: use Dio instead of stock http library to include default headers and cache bearer token

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projet_esgix/repositories/auth_repository.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:projet_esgix/services/api_service.dat.dart';
 import 'blocs/auth/auth_bloc.dart';
 import 'screens/login_screen.dart';
 
+ApiService initApiService() {
+  return ApiService(
+      baseUrl: 'https://esgix.tech',
+      defaultHeaders: {
+        'x-api-key': dotenv.get('API_KEY'),
+        'Content-Type': 'application/json'
+      });
+}
+
 Future<void> main() async {
   await dotenv.load();
-  final ApiService apiService = ApiService(baseUrl: 'https://esgix.tech', httpClient: http.Client());
+  final apiService = initApiService();
   final AuthRepository authRepository = AuthRepositoryImpl(apiService);
 
   runApp(MyApp(authRepository: authRepository));
--- lib/services/api_service.dat.dart
+++ lib/services/api_service.dat.dart
@@ -1,57 +1,68 @@
-import 'dart:convert';
 import 'dart:developer'; // Pour log
+import 'package:dio/dio.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import 'package:projet_esgix/models/user_model.dart';
 
 class ApiService {
+  static ApiService? _instance;
   final String baseUrl;
-  final http.Client httpClient;
+  Map<String, String> defaultHeaders;
+  late final Dio _httpClient;
+  late final String _token;
 
-  ApiService({required this.baseUrl, required this.httpClient});
+  ApiService._({required this.baseUrl, this.defaultHeaders = const {}})
+  {
+    _httpClient = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      headers: defaultHeaders,
+    ));
+  }
+
+  factory ApiService({required baseUrl, defaultHeaders = const {}})
+  {
+    _instance ??= ApiService._(baseUrl: baseUrl, defaultHeaders: defaultHeaders);
+
+    return _instance!;
+  }
 
   Future<User> login(String email, String password) async {
-    String apiKey = dotenv.get('API_KEY');
-    Map<String, String> headers = {
-      'x-api-key': apiKey,
-      'Content-Type': 'application/json'
-    };
-    String body = jsonEncode({'email': email, 'password': password});
-
-    final response = await httpClient.post(
-      Uri.parse('$baseUrl/auth/login'),
-      headers: headers,
-      body: body,
+    final response = await _httpClient.post(
+      '/auth/login',
+      data: {
+        'email': email,
+        'password': password
+      },
     );
 
-    if (response.statusCode == 200) {
-      return User.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to login: ${response.body}');
+    if (response.statusCode != 200) {
+      throw Exception('Failed to login: ${response.data}');
     }
+
+    _token = response.data['token'];
+
+    return User.fromJson(response.data);
   }
 
   Future<Register> register(String email, String password, String username, String avatar) async {
-    String apiKey = dotenv.get('API_KEY');
-    Map<String, String> headers = {
-      'x-api-key': apiKey,
-      'Content-Type': 'application/json'
-    };
-    String body = jsonEncode({'email': email, 'password': password, 'username': username, 'avatar': avatar});
-
-    final response = await httpClient.post(
-      Uri.parse('$baseUrl/auth/register'),
-      headers: headers,
-      body: body,
+    final response = await _httpClient.post(
+      '/auth/register',
+      data: {
+        'email': email,
+        'password': password,
+        'username': username,
+        'avatar': avatar
+      },
     );
     if (response.statusCode == 200) {
-      return Register.fromJson(json.decode(response.body));
-    } else if (response.statusCode == 400) {
-      var errorMessage = json.decode(response.body)['message'] ?? 'Unknown error occurred';
-      throw Exception(errorMessage);
+      return Register.fromJson(response.data);
+    }
+
+    if (response.statusCode == 400) {
+      throw Exception(response.data['message'] ?? 'Unknown error occurred');
     } else {
-      throw Exception('Failed to register: ${response.body}');
+      throw Exception('Failed to register: ${response.data}');
     }
   }
+
+
 }
